784a1c8ed0f6a3451aa1acde694e474ddf008f8f
hasanpasha <bashahsn22@gmail.com>
2021-12-12 17:59:31 +0300

    terminate player on main app end SIG*

--- utils/video_player.py
+++ utils/video_player.py
@@ -2,6 +2,14 @@
 import concurrent.futures
 import subprocess
 from typing import List
+import signal
+
+
+class PoliteExit(Exception):
+    pass
+
+def handle_signal(signum, frame):
+    raise PoliteExit()
 
 class MPVVideoPlayer:
     """
@@ -9,9 +17,20 @@ class MPVVideoPlayer:
         the process will be in another thread..
     """
 
+    def __init__(self) -> None:
+        # Use signal handler to throw exception which can be 
+        #+ caught to allow polite exit.
+        end_signals = [
+            signal.SIGTERM,  # 15
+            signal.SIGABRT,  # 6
+            signal.SIGQUIT,  # 3
+        ]
+        for sig in end_signals:
+            signal.signal(sig, handle_signal)
+
     def _player(self, args: List) -> bool:
         try:
-            process = subprocess.Popen(args)
+            process = self.player_process = subprocess.Popen(args)
             process.wait()
 
         except Exception as e:
@@ -30,7 +49,8 @@ class MPVVideoPlayer:
 
     def play_video(self, args: List) -> bool:
         with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-            process_args = {executor.submit(self._player, args): args}
+            process = executor.submit(self._player, args)
+            process_args = {process: args}
             try:
                 for process_future in concurrent.futures.as_completed(process_args):
                     return process_future.result()
@@ -38,3 +58,13 @@ class MPVVideoPlayer:
             # exit norammly on keyborad interrupt
             except KeyboardInterrupt:
                 return True
+
+            except PoliteExit:
+                # On receiving SIG* signal
+                self.player_process.terminate()
+
+                # And cancel the process if still running
+                process.cancel()
+
+                # End the script running, with returning error code
+                exit(3) # Quit
