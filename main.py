b64b6de8057b2be76ea5c435f0b9b1efff4053b4
hasanpasha <bashahsn22@gmail.com>
2021-12-13 22:06:13 +0300

    fix DATA_FOLDER returning link path 🐛

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ import os
 
 class Defaults:
     SERVER = 'cinemana'
-    DATA_FOLDER = os.path.join(os.getcwd(), 'data')
+    DATA_FOLDER = os.path.join(
+        os.path.split(
+            # get real path if the running file is link from this file
+            os.path.realpath(__file__)  # This will return the file path not the directory
+        )[0],   #+ so I split it and choose only the dir path
+        'data'
+        )
     SCREENSHOTS_FOLDER = os.path.join(DATA_FOLDER, 'screenshots')
     KIND = Kinds.MOVIES
 
